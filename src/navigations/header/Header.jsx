10f4e32acae34e89b16bda1f7268e533cae11703
liohumb <helloliohumb@gmail.com>
2023-03-11 12:39:40 +0100

    mise en place de la barre de navigation desktop & mobile

--- src/navigations/header/Header.jsx
+++ src/navigations/header/Header.jsx
@@ -1,9 +1,52 @@
+import { Link } from 'react-router-dom'
+
+import logo from '../../assets/images/logos/logo.png'
 import './header.scss'
 
 export default function Header(  ) {
     return (
         <header className="header">
-
+            <Link to="/" className="header__logo">
+                <img src={logo} alt=" "/>
+            </Link>
+            <nav className="header__menu">
+                <form action="" className="header__menu-search">
+                    <label htmlFor="search"></label>
+                    <input type="text" name="search" id="search" placeholder="Rechercher…"/>
+                    <button type="submit">
+                        <i className="bx bx-search"/>
+                    </button>
+                </form>
+                <Link to="/conditions-generales" className="header__menu-link">
+                    <i className="bx bx-comment-detail"/>
+                </Link>
+                <Link to="/notification" className="header__menu-link">
+                    <i className="bx bxs-bell"/>
+                </Link>
+                <Link to="/aides" className="header__menu-link">
+                    <i className="bx bx-help-circle"/>
+                </Link>
+                <Link to="/profil/:id" className="header__menu-link">
+                    <i className="bx bxs-user"/>
+                </Link>
+            </nav>
+            <nav className="header__bottom">
+                <Link to="/" className="header__bottom-link">
+                    <i className="bx bxs-home"/>
+                </Link>
+                <Link to="/rechercher" className="header__bottom-link">
+                    <i className="bx bxs-search"/>
+                </Link>
+                <Link to="/collections" className="header__bottom-link">
+                    <i className="bx bxs-collection"/>
+                </Link>
+                <Link to="/chat" className="header__bottom-link">
+                    <i className="bx bxs-chat"/>
+                </Link>
+                <Link to="/profil/:id" className="header__bottom-link">
+                    <i className="bx bxs-user"/>
+                </Link>
+            </nav>
         </header>
     )
 }
