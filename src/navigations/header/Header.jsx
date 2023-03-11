a6a7edfeacb9dc934fbfc10a763712380bb41f1d
liohumb <helloliohumb@gmail.com>
2023-03-11 13:07:53 +0100

    ajout du context user & img profil user

--- src/navigations/header/Header.jsx
+++ src/navigations/header/Header.jsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Context } from '../../context/Context'
 
 import logo from '../../assets/images/logos/logo.png'
+import profil from '../../assets/images/user/profil.png'
 import './header.scss'
 
 export default function Header(  ) {
+    const {user} = useContext(Context)
+    
     return (
         <header className="header">
             <Link to="/" className="header__logo">
@@ -26,9 +31,15 @@ export default function Header(  ) {
                 <Link to="/aides" className="header__menu-link">
                     <i className="bx bx-help-circle"/>
                 </Link>
-                <Link to="/profil/:id" className="header__menu-link">
-                    <i className="bx bxs-user"/>
-                </Link>
+                {user ?
+                    <Link to="/profil/:id" className="header__menu-link">
+                        <img src={profil} alt={user.name}/>
+                    </Link>
+                :
+                    <Link to="/connexion" className="header__menu-link">
+                        <i className="bx bxs-user"/>
+                    </Link>
+                }
             </nav>
             <nav className="header__bottom">
                 <Link to="/" className="header__bottom-link">
@@ -43,9 +54,15 @@ export default function Header(  ) {
                 <Link to="/chat" className="header__bottom-link">
                     <i className="bx bxs-chat"/>
                 </Link>
-                <Link to="/profil/:id" className="header__bottom-link">
-                    <i className="bx bxs-user"/>
-                </Link>
+                {user ?
+                    <Link to="/profil/:id" className="header__bottom-link">
+                        <img src={profil} alt={user.name}/>
+                    </Link>
+                :
+                    <Link to="/connexion" className="header__bottom-link">
+                        <i className="bx bxs-user"/>
+                    </Link>
+                }
             </nav>
         </header>
     )
