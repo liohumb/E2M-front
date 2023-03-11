c20a1f79ce350d60460570230e879cbead7ac9a3
liohumb <helloliohumb@gmail.com>
2023-03-11 10:17:14 +0100

    reset project & ready to develop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useContext } from 'react'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { Context } from './context/Context'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './navigations/header/Header'
+import Login from './authentification/login/Login'
+import Register from './authentification/register/Register'
+import Home from './pages/home/Home'
+
+import './assets/styles/style.scss'
+
+export default function App() {
+    const { user } = useContext( Context )
+    const { pathname, hash, key } = useLocation()
+
+    useEffect( () => {
+        if (hash === '') {
+            window.scrollTo( 0, 0 )
+        } else {
+            setTimeout( () => {
+                const id = hash.replace( '#', '' )
+                const elem = document.getElementById( id )
+
+                if (elem) {
+                    elem.scrollIntoView()
+                }
+            }, 0 )
+        }
+    }, [pathname, hash, key] )
+
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route path="/inscription/:token" element={<Register/>}/>
+                <Route path="/connexion" element={user ? <Navigate to="/"/> : <Login/>}/>
+                <Route index path="/" element={<Home/>}/>
+            </Routes>
+        </>
+    )
 }
-
-export default App;
